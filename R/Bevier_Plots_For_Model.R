56a9c9d63fe7810bf90ebbfcaabcc24a3491bc0e
Gustavo Reyes <gareyes3@illinois.edu>
2023-05-12 13:53:09 -0500

    model updates

---
+++ R/Bevier_Plots_For_Model.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(reshape2)
+
+
+Bevier_Trial_For_Model<-read.csv("Bevier_Trial_Temps_formodel.csv")
+Bevier_Trial_For_Model_Melted<-melt(Bevier_Trial_For_Model,id.vars = "Time")
+
+Bevier_Trial_For_Model_Melted %>% 
+  ggplot(aes(x = Time , y  = value))+
+  geom_point(color = "skyblue")+
+  geom_line()+
+  facet_wrap(~variable ,nrow = 5)+
+  theme_bw()+
+  labs(y = "Temperature (°C", x = "Time (min)")
+
